f477dbeb8bc6c471b7b9f859569d167ca6f7478f
Goran Todorovic <goran375@yahoo.com>
2016-01-20 23:56:47 +0100

    Added dependencies for Sequelize framework and initial settings for MySQL database. Added server logging.

--- index.js
+++ index.js
@@ -2,27 +2,72 @@
 'use strict';
 
 var Hapi = require('hapi');
+const Good = require('good');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+	host: 'localhost', 
+	port: 8000 
 });
 
-// Add the route
+// Add the route (test)
 server.route({
-    method: 'GET',
-    path:'/hi', 
-    handler: function (request, reply) {
-        return reply('Hi! Testing! :)');
-    }
+	method: 'GET',
+	path:'/hi', 
+	handler: function (request, reply) {
+		return reply('Hi! Testing! :)');
+	}
 });
 
-// Start the server
-server.start((err) => {
-    if (err) {
-        throw err;
+// Database test
+server.register(
+	[
+		{
+			register: require('hapi-sequelize'),
+			options: {
+				database: 'sep_pcc',
+				user: 'root',
+				pass: '',
+				dialect: 'mysql',
+				port: 3306,
+				models: 'models/**/*.js',
+				sequelize: {
+					define: {
+						underscoredAll: true
+					}
+				}
+			}
+		},
+	], function(err) {
+		if (err) {
+			console.error('Failed to load hapi-sequelize plugin!');
+		}
+	}
+);
+
+// Log 
+server.register({
+	register: Good,
+	options: {
+		reporters: [{
+			reporter: require('good-console'),
+			events: {
+				response: '*',
+				log: '*'
+			}
+		}]
+	}
+}, (err) => {
+	if (err) {
+		throw err; // Failed loading "good" plugin!
     }
-    console.log('Server running at:', server.info.uri);
+
+	// Start the server
+	server.start((err) => {
+		if (err) {
+			throw err;
+		}
+		console.log('Server running at:', server.info.uri);
+	});
 });
